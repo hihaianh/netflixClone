ee9c047ece80e79b584c95cf8c2084f11f2b7587
Anh <97702902+hihaianh@users.noreply.github.com>
2024-09-02 23:17:01 +0100

    Uninstalled router (only need one page), installed tailwind packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,30 +1,11 @@
-import ReactDOM from 'react-dom/client';
 import React from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
-import Root from "./routes/root";
-import ErrorPage from "./ErrorPage.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'; 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//     <RouterProvider router={router} />
-//   </StrictMode>,
-// )
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
