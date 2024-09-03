dad76e2a9a1c273e4a5b1812caccabe8b85de444
Anh <97702902+hihaianh@users.noreply.github.com>
2024-09-04 00:21:02 +0100

    installed jest and making fixes to endpoints

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY;
@@ -13,44 +13,79 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-const apiClient = async (baseUrl, path, queryParams) => {
-    const url = new URL(`${baseUrl}${path}`)
+// Key = 2280056 ... location key of Europe's London
 
-    if (queryParams) {
-        url.search = new URLSearchParams(queryParams);
-    }
+// get city
+const grabCity = async (city) => {
+    const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const queryParams = `?apikey=${apiKey}&q=${city}`
 
-    const response = await fetch(url.toString(), {
-        method: "GET"
-    })
+    const response = await fetch(baseUrl + queryParams)
+    const data = await response.json();
 
-    const isResponseJson = response.headers
-        .get("Content-Type")
-        .includes("application/json");
+    // get location key to search for ten day forecast
+    return data[0]?.Key;
 
-    const result = isResponsejson ? await response.json() : await response.text();
-    return result;
 }
 
+// get 10-day weather forecast results from city
+const grabTenDayForecast = async (locationKey) => {
+    const baseUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/10day/";
+    const query = `${locationKey}?apikey=${apiKey}`
+
+    const response = await fetch(baseUrl + query);
+    const data = await response.json();
+
+    console.log(data);
+
+}
+
+grabTenDayForecast("2280056");
+
+grabCity("london").then(data => {
+    return grabTenDayForecast(data.Key)
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
+
+// const apiClient = async (baseUrl, path, queryParams) => {
+//     const url = new URL(`${baseUrl}${path}`)
+
+//     if (queryParams) {
+//         url.search = new URLSearchParams(queryParams);
+//     }
+
+//     const response = await fetch(url.toString(), {
+//         method: "GET"
+//     })
+
+//     const isResponseJson = response.headers
+//         .get("Content-Type")
+//         .includes("application/json");
+
+//     const result = isResponsejson ? await response.json() : await response.text();
+//     return result;
+// }
+
 // searching for location key that is needed to retrieve the city's forecast data
-app.get('/', async (req, res) => {
-    const city = req.query.q;
+// app.get('/', async (req, res) => {
+//     const city = req.query.q;
     
-    const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
+//     const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
 
-    try {
-        const [results] = await apiClient(baseUrl, {
-            ...(city ? {q} : {q: "London"}),
-            apikey: apiKey
-        })
+//     try {
+//         const [results] = await apiClient(baseUrl, {
+//             ...(city ? {q} : {q: "London"}),
+//             apikey: apiKey
+//         })
 
-        return res.status(200).json(results)
+//         return res.status(200).json(results)
 
-    } catch (err) {
-        console.log(err);
-        return res.status(400).json({message: "Failed to retrieve weather data: ", err})
-    }
-})
+//     } catch (err) {
+//         console.log(err);
+//         return res.status(400).json({message: "Failed to retrieve weather data: ", err})
+//     }
+// })
 
 // 10 days of daily forecasts - london - locationKey = 328328
 
---
+++ server/forecast.js
@@ -0,0 +1,16 @@
+// require("dotenv").config();
+
+// const key = process.env.API_KEY;
+
+// const grabCity = async (city) => {
+//     const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
+//     const queryParams = `?apikey=${key}&q=${city}`
+
+//     const response = await fetch(baseUrl + queryParams)
+//     const data = await response.json();
+
+//     console.log(data);
+
+// }
+
+// grabCity("london");
